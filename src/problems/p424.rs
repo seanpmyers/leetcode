49f757cd96c7d460e336eb6d3c2277681b513dce
Sean Myers <spmyers@protonmail.com>
2024-11-02 18:16:40 -0400

    updating

--- src/problems/p424.rs
+++ src/problems/p424.rs
@@ -1,8 +1,31 @@
 pub struct Solution {}
 impl Solution {
     pub fn character_replacement(s: String, k: i32) -> i32 {
+        if k == s.len() as i32 {
+            return k;
+        }
         let mut result: i32 = 1;
+        let chars: Vec<char> = s.chars().collect();
+        let mut s: usize = 0;
+        let mut e: usize = 0;
+        let mut current: i32 = k;
+        while e < chars.len() {
+            if chars[e] != chars[s] {
+                if current > 0 {
+                    current -= 1;
+                    e += 1;
+                    continue;
+                }
+                current = k;
+                result = result.max(chars[s..e].len() as i32);
+                println!("{} {} {:?}", s, e, chars[s..e].to_vec());
+                s += 1;
+                e = s + 1;
+                continue;
+            }
+            e += 1;
+        }
 
-        result
+        result.max(chars[s..e].len() as i32)
     }
 }
