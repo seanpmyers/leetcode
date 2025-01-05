5ec1b2be6b2d47c579b92de8663735ee9175aeb7
Sean Myers <22840861+seanpmyers@users.noreply.github.com>
2025-01-05 11:58:36 -0500

    fixing palindrome solution

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,44 @@ fn main() {
 
 pub struct Solution;
 impl Solution {
+    pub fn is_palindrome(s: String) -> bool {
+        const LOWER_A: u8 = b'a' as u8;
+        const LOWER_Z: u8 = b'z' as u8;
+        const UPPER_A: u8 = b'A' as u8;
+        const UPPER_Z: u8 = b'Z' as u8;
+        const ZERO: u8 = b'0' as u8;
+        const NINE: u8 = b'9' as u8;
+        pub fn is_valid_char(c: u8) -> bool {
+            c >= LOWER_A && c <= LOWER_Z || c >= UPPER_A && c <= UPPER_Z || c >= ZERO && c <= NINE
+        }
+        pub fn to_lowercase(mut c: u8) -> u8 {
+            if c >= UPPER_A && c <= UPPER_Z {
+                c = c + (LOWER_A - UPPER_A);
+            }
+            c
+        }
+
+        let mut l: usize = 0;
+        let mut r: usize = s.len() - 1;
+        let chars = s.as_bytes();
+        while l < r {
+            while l < r && !is_valid_char(chars[l]) {
+                l = l.saturating_add(1);
+            }
+            while r > l && !is_valid_char(chars[r]) {
+                r = r.saturating_sub(1);
+            }
+
+            if to_lowercase(chars[l]) != to_lowercase(chars[r]) {
+                return false;
+            }
+            l = l.saturating_add(1);
+            r = r.saturating_sub(1);
+        }
+
+        true
+    }
+
     pub fn longest_consecutive(nums: Vec<i32>) -> i32 {
         use std::collections::HashMap;
         let mut longest: i32 = 0;
--- src/problems/p125.rs
+++ src/problems/p125.rs
@@ -1,36 +1,40 @@
 pub struct Solution {}
 impl Solution {
     pub fn is_palindrome(s: String) -> bool {
-        let s: Vec<char> = s
-            .chars()
-            .filter(|c| is_ascii_alpha_numeric(*c as usize))
-            .map(|c| c.to_ascii_lowercase())
-            .collect::<Vec<char>>();
-        if s.is_empty() || s.len() == 1 {
-            return true;
+        const LOWER_A: u8 = b'a' as u8;
+        const LOWER_Z: u8 = b'z' as u8;
+        const UPPER_A: u8 = b'A' as u8;
+        const UPPER_Z: u8 = b'Z' as u8;
+        const ZERO: u8 = b'0' as u8;
+        const NINE: u8 = b'9' as u8;
+        pub fn is_valid_char(c: u8) -> bool {
+            c >= LOWER_A && c <= LOWER_Z || c >= UPPER_A && c <= UPPER_Z || c >= ZERO && c <= NINE
+        }
+        pub fn to_lowercase(mut c: u8) -> u8 {
+            if c >= UPPER_A && c <= UPPER_Z {
+                c = c + (LOWER_A - UPPER_A);
+            }
+            c
         }
 
-        let end: usize = s.len() - 1;
-        let middle: usize = s.len() / 2;
-        for i in 0..s.len() {
-            if i == middle {
-                break;
+        let mut l: usize = 0;
+        let mut r: usize = s.len() - 1;
+        let chars = s.as_bytes();
+        while l < r {
+            while l < r && !is_valid_char(chars[l]) {
+                l = l.saturating_add(1);
             }
-            if s[i] != s[end - i] {
+            while r > l && !is_valid_char(chars[r]) {
+                r = r.saturating_sub(1);
+            }
+
+            if to_lowercase(chars[l]) != to_lowercase(chars[r]) {
                 return false;
             }
+            l = l.saturating_add(1);
+            r = r.saturating_sub(1);
         }
+
         true
     }
-}
-
-// there is a built in method but here is an example of how you would wirte a method
-pub fn is_ascii_alpha_numeric(input: usize) -> bool {
-    let a: usize = 'a' as usize;
-    let z: usize = 'z' as usize;
-    let big_a: usize = 'A' as usize;
-    let big_z: usize = 'Z' as usize;
-    let zero: usize = '0' as usize;
-    let nine: usize = '9' as usize;
-    input >= a && input <= z || input >= big_a && input <= big_z || input >= zero && input <= nine
 }
