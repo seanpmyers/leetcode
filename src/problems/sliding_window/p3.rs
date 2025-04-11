33c68fd864d8f6a3b0bb163550cbd2b86a2edb55
Sean Myers <spmyers@protonmail.com>
2025-04-11 13:08:28 -0400

    Update p3.rs

--- src/problems/sliding_window/p3.rs
+++ src/problems/sliding_window/p3.rs
@@ -1,8 +1,8 @@
 pub struct Solution {}
 
-use std::collections::HashSet;
 impl Solution {
     pub fn length_of_longest_substring(s: String) -> i32 {
+        use std::collections::HashSet;
         if s.is_empty() {
             return 0i32;
         }
@@ -19,5 +19,24 @@ impl Solution {
             result = result.max(set.len() as i32);
         }
         result.max(set.len() as i32)
+    }
+
+    pub fn hash_map(bytes: &[u8]) -> i32 {
+        use std::collections::HashMap;
+        if bytes.is_empty() {
+            return 0i32;
+        }
+        let mut result: usize = 1usize;
+        let mut l: usize = 0usize;
+        let mut map: HashMap<u8, usize> = HashMap::with_capacity(bytes.len());
+        for r in 0..bytes.len() {
+            if let Some(value) = map.get(&bytes[r]) {
+                l = l.max(value + 1);
+            }
+            map.insert(bytes[r], r);
+            result = result.max(r - l + 1);
+        }
+
+        result as i32
     }
 }
