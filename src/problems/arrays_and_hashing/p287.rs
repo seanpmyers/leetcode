5b34a07c6ae2a988cabb48f4e5a7395496261225
seanpmyers <spmyers@protonmail.com>
2025-10-21 17:57:08 -0400

    updating p287

--- src/problems/arrays_and_hashing/p287.rs
+++ src/problems/arrays_and_hashing/p287.rs
@@ -1,25 +1,20 @@
 pub struct Solution;
 impl Solution {
     pub fn find_duplicate(nums: Vec<i32>) -> i32 {
-        if nums.len() == 2 {
-            return nums[0];
-        }
-        let mut slow: usize = nums[0] as usize;
-        let mut fast: usize = nums[nums[0] as usize] as usize;
-        loop {
+        let mut slow: usize = nums[0usize] as usize;
+        let mut fast: usize = nums[nums[0usize] as usize] as usize;
+
+        while slow != fast {
             slow = nums[slow] as usize;
             fast = nums[nums[fast] as usize] as usize;
-            if slow == fast {
-                break;
-            }
         }
-        let mut slow2: usize = 0;
-        loop {
+
+        let mut slow2: usize = 0usize;
+
+        while slow != slow2 {
             slow = nums[slow] as usize;
             slow2 = nums[slow2] as usize;
-            if slow == slow2 {
-                return slow as i32;
-            }
         }
+        return slow as i32;
     }
 }
