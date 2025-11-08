cb5a6bcf9726f09e72f37cd808b6ae4bdd22d973
seanpmyers <spmyers@protonmail.com>
2025-11-08 14:32:59 -0500

    adding incomplete solution p572

--- src/problems/trees/p572.rs
+++ src/problems/trees/p572.rs
@@ -61,3 +61,76 @@ pub mod dfs {
         }
     }
 }
+
+pub mod serialization {
+    pub struct Solution;
+    #[derive(Debug, PartialEq, Eq)]
+    pub struct TreeNode {
+        pub val: i32,
+        pub left: Option<Rc<RefCell<TreeNode>>>,
+        pub right: Option<Rc<RefCell<TreeNode>>>,
+    }
+
+    impl TreeNode {
+        #[inline]
+        pub fn new(val: i32) -> Self {
+            TreeNode {
+                val,
+                left: None,
+                right: None,
+            }
+        }
+    }
+    use std::cell::RefCell;
+    use std::rc::Rc;
+    impl Solution {
+        pub fn is_subtree(
+            root: Option<Rc<RefCell<TreeNode>>>,
+            sub_root: Option<Rc<RefCell<TreeNode>>>,
+        ) -> bool {
+            let Some(root) = root else {
+                return false;
+            };
+
+            let Some(sub_root) = sub_root else {
+                return false;
+            };
+
+            let serialized_root: String = Self::serialize(Some(root.clone()));
+            let serialized_sub_root: String = Self::serialize(Some(sub_root.clone()));
+            let combined = format!("{}|{}", serialized_root, serialized_sub_root);
+
+            let z_values = Self::z_function(combined);
+
+            false
+        }
+
+        pub fn z_function(input: String) -> Vec<i32> {
+            let mut result: Vec<i32> = vec![0; 2000usize];
+            let mut l: usize = 0;
+            let mut r: usize = 0;
+            let mut n: usize = input.len();
+
+            for i in 0..n {
+                if i <= r {
+                    result[i] = result[i + result[i] as usize].min((r - i + 1) as i32);
+                }
+            }
+
+            result
+        }
+
+        pub fn serialize(root: Option<Rc<RefCell<TreeNode>>>) -> String {
+            let Some(root) = root else {
+                return format!("$#");
+            };
+
+            return format!(
+                "${}{}{}",
+                root.borrow().val,
+                Self::serialize(root.borrow().left.clone()),
+                Self::serialize(root.borrow().right.clone())
+            );
+        }
+    }
+}
