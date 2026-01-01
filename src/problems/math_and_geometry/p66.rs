7bebfdd82d908d0319f61a1e7fcb670761f8dbb6
seanpmyers <spmyers@protonmail.com>
2026-01-01 11:25:42 -0500

    adding solution variants to p66

--- src/problems/math_and_geometry/p66.rs
+++ src/problems/math_and_geometry/p66.rs
@@ -1,18 +1,77 @@
-pub struct Solution {}
-impl Solution {
-    pub fn plus_one(mut digits: Vec<i32>) -> Vec<i32> {
-        for i in (0..digits.len()).rev() {
-            if digits[i] == 9 {
+pub mod option_1 {
+    pub struct Solution {}
+    impl Solution {
+        pub fn plus_one(mut digits: Vec<i32>) -> Vec<i32> {
+            for i in (0..digits.len()).rev() {
+                if digits[i] == 9 {
+                    digits[i] = 0;
+                    if i == 0 {
+                        digits.insert(0, 1);
+                        return digits;
+                    }
+                    continue;
+                }
+                digits[i] += 1;
+                return digits;
+            }
+            digits
+        }
+    }
+}
+
+pub mod option_2 {
+    pub struct Solution {}
+    impl Solution {
+        pub fn plus_one(mut digits: Vec<i32>) -> Vec<i32> {
+            for i in (0..digits.len()).rev() {
+                if digits[i] != 9 {
+                    digits[i] += 1;
+                    return digits;
+                }
+
                 digits[i] = 0;
+                if i != 0 {
+                    continue;
+                }
+
+                digits[i] += 1;
+                return digits;
+            }
+
+            digits
+        }
+    }
+}
+
+pub mod option_3 {
+    pub struct Solution {}
+    impl Solution {
+        pub fn plus_one(mut digits: Vec<i32>) -> Vec<i32> {
+            for i in (0..digits.len()).rev() {
+                let Some(x) = digits.get_mut(i) else {
+                    break;
+                };
+
+                if x.saturating_add(1) <= 9 {
+                    *x = x.saturating_add(1);
+                    return digits;
+                }
+
                 if i == 0 {
+                    *x = 0;
                     digits.insert(0, 1);
                     return digits;
                 }
-                continue;
+
+                if x.saturating_add(1) <= 9 {
+                    *x = x.saturating_add(1);
+                    return digits;
+                }
+
+                *x = 0;
             }
-            digits[i] += 1;
-            return digits;
+
+            digits
         }
-        digits
     }
 }
