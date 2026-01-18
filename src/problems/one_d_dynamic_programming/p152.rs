5874015dbeff8434d0121810f6fdd460bce1d471
seanpmyers <spmyers@protonmail.com>
2026-01-18 10:09:36 -0500

    p152 adding kadane

--- src/problems/one_d_dynamic_programming/p152.rs
+++ src/problems/one_d_dynamic_programming/p152.rs
@@ -1,3 +1,30 @@
+pub mod kadane {
+    pub struct Solution;
+    impl Solution {
+        pub fn max_product(nums: Vec<i32>) -> i32 {
+            if nums.len() == 1 {
+                return nums[0];
+            }
+
+            let mut result: i32 = nums[0];
+
+            let mut max: i32 = 1i32;
+            let mut min: i32 = 1i32;
+
+            for number in nums.iter() {
+                let product: i32 = number * max;
+
+                max = product.max(*number).max(number * min);
+                min = product.min(*number).min(number * min);
+
+                result = result.max(max);
+            }
+
+            result
+        }
+    }
+}
+
 pub mod first_attempt {
     pub struct Solution;
     impl Solution {
@@ -11,7 +38,7 @@ pub mod first_attempt {
 
             for i in 0..len {
                 let mut product: i32 = nums[i];
-                let mut reverse: i32 = nums[len.saturating_sub(i)];
+                let mut reverse: i32 = nums[len.saturating_sub(1 + i)];
                 result = result.max(product).max(reverse);
                 for j in i.saturating_add(1)..len {
                     product *= nums[j];
