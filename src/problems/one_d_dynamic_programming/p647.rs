34c5f0d7dbb85c6dd6f044812adc25735e6d42dd
seanpmyers <spmyers@protonmail.com>
2026-01-31 10:52:05 -0500

    adding p647 manacher solution

--- src/problems/one_d_dynamic_programming/p647.rs
+++ src/problems/one_d_dynamic_programming/p647.rs
@@ -1,3 +1,50 @@
+pub mod manacher {
+    pub struct Solution;
+    impl Solution {
+        pub fn count_substrings(s: String) -> i32 {
+            if s.is_empty() {
+                return 0i32;
+            }
+
+            let p = Self::manacher(s.as_bytes());
+
+            p.iter().map(|x| ((x + 1) / 2) as i32).sum::<i32>()
+        }
+
+        pub fn manacher(bytes: &[u8]) -> Vec<usize> {
+            let mut values: Vec<u8> = Vec::with_capacity((bytes.len() * 2) + 1);
+            values.push(b'#');
+            for i in 0..bytes.len() {
+                values.push(bytes[i]);
+                values.push(b'#');
+            }
+            let n: usize = values.len();
+            let mut p: Vec<usize> = vec![0usize; n];
+
+            let (mut l, mut r): (usize, usize) = (0, 0);
+
+            for i in 0..n {
+                if i < r {
+                    p[i] = (r - i).min(p[l + (r - i)]);
+                }
+
+                while i + 1 + p[i] < n
+                    && i >= p[i] + 1
+                    && values[i - p[i] - 1] == values[i + p[i] + 1]
+                {
+                    p[i] += 1;
+                }
+
+                if p[i] + i > r {
+                    l = i - p[i];
+                    r = i + p[i];
+                }
+            }
+
+            p
+        }
+    }
+}
 pub mod cubic {
     pub struct Solution;
     impl Solution {
