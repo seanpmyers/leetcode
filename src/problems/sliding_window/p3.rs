ef18c9e406840a36f75017f6f153def434415884
seanpmyers <spmyers@protonmail.com>
2026-02-04 06:49:14 -0500

    updating p3

--- src/problems/sliding_window/p3.rs
+++ src/problems/sliding_window/p3.rs
@@ -1,27 +1,35 @@
-pub struct Solution {}
-
-impl Solution {
-    pub fn length_of_longest_substring(s: String) -> i32 {
-        use std::collections::HashSet;
-        if s.is_empty() {
-            return 0i32;
-        }
-        let chars: Vec<char> = s.chars().collect();
-        let mut result: i32 = 1i32;
-        let mut l: usize = 0;
-        let mut set: HashSet<char> = HashSet::new();
-        for r in 0..chars.len() {
-            while set.contains(&chars[r]) {
-                set.remove(&chars[l]);
-                l += 1;
+pub mod linear_sliding_window {
+    pub struct Solution {}
+    impl Solution {
+        pub fn length_of_longest_substring(s: String) -> i32 {
+            let s: &[u8] = s.as_bytes();
+            use std::collections::HashSet;
+            if s.is_empty() {
+                return 0i32;
             }
-            set.insert(chars[r]);
-            result = result.max(set.len() as i32);
-        }
-        result.max(set.len() as i32)
-    }
 
-    pub fn hash_map(bytes: &[u8]) -> i32 {
+            let mut result: i32 = 1i32;
+            let mut set: HashSet<u8> = HashSet::with_capacity(s.len());
+            let mut l: usize = 0;
+
+            for r in 0..s.len() {
+                while set.contains(&s[r]) {
+                    set.remove(&s[l]);
+                    l += 1;
+                }
+                set.insert(s[r]);
+                result = result.max(set.len() as i32);
+            }
+
+            result
+        }
+    }
+}
+
+pub mod hash_map {
+    pub struct Solution;
+    pub fn length_of_longest_substring(s: String) -> i32 {
+        let bytes: &[u8] = s.as_bytes();
         use std::collections::HashMap;
         if bytes.is_empty() {
             return 0i32;
@@ -38,5 +46,36 @@ impl Solution {
         }
 
         result as i32
+    }
+}
+
+pub mod quadtraic_sliding_window {
+    pub struct Solution;
+    impl Solution {
+        pub fn length_of_longest_substring(s: String) -> i32 {
+            let mut result: i32 = 0i32;
+            if s.len() == 1 {
+                return 1i32;
+            }
+            use std::collections::HashSet;
+            let mut set: HashSet<u8> = HashSet::with_capacity(s.len());
+            let mut l: usize = 0;
+            let mut r: usize = 1;
+            let s: &[u8] = s.as_bytes();
+            while r < s.len() && l < r {
+                set.insert(s[l]);
+                while r < s.len() && set.insert(s[r]) {
+                    r += 1;
+                }
+                result = result.max((r - l) as i32);
+                set.clear();
+                l = l + 1;
+                if r < s.len() {
+                    r = l + 1;
+                }
+            }
+
+            result
+        }
     }
 }
