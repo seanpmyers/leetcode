a49fad6b9f0d04cefd768fd927c13c5f5d6091f4
seanpmyers <spmyers@protonmail.com>
2026-02-04 06:49:23 -0500

    updating p121

--- src/problems/sliding_window/p121.rs
+++ src/problems/sliding_window/p121.rs
@@ -1,14 +1,38 @@
-pub struct Solution {}
+pub mod dynamic {
+    pub struct Solution {}
+    impl Solution {
+        pub fn max_profit(prices: Vec<i32>) -> i32 {
+            let mut result: i32 = 0;
+            let mut current = prices[0];
+            for v in prices.into_iter() {
+                current = current.min(v);
+                result = result.max(v - current);
+            }
 
-impl Solution {
-    pub fn max_profit(prices: Vec<i32>) -> i32 {
-        let mut result: i32 = 0;
-        let mut current = prices[0];
-        for v in prices.into_iter() {
-            current = current.min(v);
-            result = result.max(v - current);
+            result
         }
+    }
+}
+pub mod sliding_window {
+    pub struct Solution {}
+    impl Solution {
+        pub fn max_profit(prices: Vec<i32>) -> i32 {
+            let mut result: i32 = 0i32;
 
-        result
+            let mut l: usize = 0;
+            let mut r: usize = 1;
+
+            while r < prices.len() {
+                let left = prices[l];
+                let right = prices[r];
+                match left < right {
+                    true => result = result.max(right - left),
+                    false => l = r,
+                };
+                r += 1;
+            }
+
+            result
+        }
     }
 }
