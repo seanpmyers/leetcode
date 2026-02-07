c37fe6e0f8d2a01c5cff5b075b9b06dfe159d78f
seanpmyers <spmyers@protonmail.com>
2026-02-07 09:19:15 -0500

    adding p678 greedy

--- src/problems/greedy/p678.rs
+++ src/problems/greedy/p678.rs
@@ -1,3 +1,38 @@
+pub mod greedy {
+    pub struct Solution;
+    impl Solution {
+        pub fn check_valid_string(s: String) -> bool {
+            let s = s.as_bytes();
+            let mut min: u8 = 0;
+            let mut max: i16 = 0;
+
+            for i in 0..s.len() {
+                match s[i] {
+                    b'(' => {
+                        min += 1;
+                        max += 1;
+                    }
+                    b'*' => {
+                        max += 1;
+                        min = min.saturating_sub(1);
+                    }
+                    b')' => {
+                        min = min.saturating_sub(1);
+                        max -= 1;
+                    }
+                    _ => panic!("invalid state"),
+                };
+
+                if max < 0 {
+                    return false;
+                }
+            }
+
+            min == 0
+        }
+    }
+}
+
 pub mod stack {
     pub struct Solution;
     impl Solution {
