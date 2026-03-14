c5cc9def24500300ca4e016c1ead9d50540464be
seanpmyers <spmyers@protonmail.com>
2026-03-14 08:28:14 -0400

    adding p206 recursive

--- src/problems/linked_list/p206.rs
+++ src/problems/linked_list/p206.rs
@@ -10,59 +10,86 @@ impl ListNode {
         ListNode { next: None, val }
     }
 }
+pub mod recursive {
+    use crate::problems::linked_list::p206::ListNode;
 
-pub struct Solution {}
+    pub struct Solution;
+    impl Solution {
+        pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+            let Some(mut head) = head else {
+                return None;
+            };
+            Some(Self::recurse(head.next.take(), head))
+        }
 
-impl Solution {
-    pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-        impl_1(head)
+        pub fn recurse(node: Option<Box<ListNode>>, head: Box<ListNode>) -> Box<ListNode> {
+            let Some(mut node) = node else {
+                return head;
+            };
+
+            let next = node.next.take();
+            node.next = Some(head);
+            Self::recurse(next, node)
+        }
     }
 }
 
-#[allow(dead_code)]
-pub fn impl_1(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-    if head.is_none() {
-        return head;
+pub mod misc {
+    use crate::problems::linked_list::p206::ListNode;
+
+    pub struct Solution {}
+
+    impl Solution {
+        pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+            impl_1(head)
+        }
     }
 
-    let mut result: Option<Box<ListNode>> = None;
-    while let Some(mut node) = head {
-        let temp = node.as_mut();
-        let next = temp.next.take();
-        head = next;
-        temp.next = result.take();
-        result = Some(node);
+    #[allow(dead_code)]
+    pub fn impl_1(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        if head.is_none() {
+            return head;
+        }
+
+        let mut result: Option<Box<ListNode>> = None;
+        while let Some(mut node) = head {
+            let temp = node.as_mut();
+            let next = temp.next.take();
+            head = next;
+            temp.next = result.take();
+            result = Some(node);
+        }
+
+        result
     }
 
-    result
-}
+    #[allow(dead_code)]
+    pub fn impl_2(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        if head.is_none() {
+            return head;
+        }
 
-#[allow(dead_code)]
-pub fn impl_2(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-    if head.is_none() {
-        return head;
+        let mut current = &mut head.as_mut()?.next.take();
+
+        while current.is_some() {
+            let mut next = current.as_mut()?.next.take();
+            current = current;
+            std::mem::swap(&mut head, &mut current.as_mut()?.next);
+            std::mem::swap(&mut head, current);
+            std::mem::swap(current, &mut next);
+        }
+
+        head
     }
 
-    let mut current = &mut head.as_mut()?.next.take();
-
-    while current.is_some() {
-        let mut next = current.as_mut()?.next.take();
-        current = current;
-        std::mem::swap(&mut head, &mut current.as_mut()?.next);
-        std::mem::swap(&mut head, current);
-        std::mem::swap(current, &mut next);
+    #[allow(dead_code)]
+    pub fn impl_3(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut result = None;
+        while let Some(mut node) = head {
+            head = node.next.take();
+            node.next = result.take();
+            result = Some(node);
+        }
+        result
     }
-
-    head
-}
-
-#[allow(dead_code)]
-pub fn impl_3(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-    let mut result = None;
-    while let Some(mut node) = head {
-        head = node.next.take();
-        node.next = result.take();
-        result = Some(node);
-    }
-    result
 }
