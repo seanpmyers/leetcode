21e32084c835de3e777c42fa7911ecc98506ded1
seanpmyers <spmyers@protonmail.com>
2026-04-01 18:39:19 -0400

    adding p146 solution and updating history

--- history.md
+++ history.md
@@ -1,1 +1,10 @@
 
+- 2026.04.01 - hashing
+- merge sort
+- quick sort
+- heaps
+- backtracking
+- morris traversal
+- dfs iterative
+- bfs iterative
+
--- src/problems/linked_list/p146.rs
+++ src/problems/linked_list/p146.rs
@@ -1,146 +1,202 @@
-use std::cell::RefCell;
-use std::collections::HashMap;
-use std::rc::Rc;
+pub mod double_link_list {
+    use std::cell::RefCell;
+    use std::collections::HashMap;
+    use std::rc::Rc;
 
-pub type LinkedListNode = Rc<RefCell<Node>>;
+    pub type LinkedListNode = Rc<RefCell<Node>>;
 
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Node {
-    pub key: i32,
-    pub value: i32,
-    pub previous: Option<LinkedListNode>,
-    pub next: Option<LinkedListNode>,
-}
+    #[derive(Debug, Clone, PartialEq, Eq)]
+    pub struct Node {
+        pub key: i32,
+        pub value: i32,
+        pub previous: Option<LinkedListNode>,
+        pub next: Option<LinkedListNode>,
+    }
 
-#[derive(Debug, Clone)]
-pub struct LRUCache {
-    pub capacity: usize,
-    pub map: HashMap<i32, LinkedListNode>,
-    pub head: Option<LinkedListNode>,
-    pub tail: Option<LinkedListNode>,
-}
+    #[derive(Debug, Clone)]
+    pub struct LRUCache {
+        pub capacity: usize,
+        pub map: HashMap<i32, LinkedListNode>,
+        pub head: Option<LinkedListNode>,
+        pub tail: Option<LinkedListNode>,
+    }
 
-impl Node {
-    pub fn new(key: i32, value: i32) -> LinkedListNode {
-        Rc::new(RefCell::new(Node {
-            key,
-            value,
-            previous: None,
-            next: None,
-        }))
+    impl Node {
+        pub fn new(key: i32, value: i32) -> LinkedListNode {
+            Rc::new(RefCell::new(Node {
+                key,
+                value,
+                previous: None,
+                next: None,
+            }))
+        }
+    }
+
+    #[allow(dead_code)]
+    impl LRUCache {
+        const NOT_FOUND: i32 = -1i32;
+
+        fn new(capacity: i32) -> Self {
+            let capacity: usize = capacity as usize;
+            Self {
+                capacity,
+                map: HashMap::with_capacity(capacity),
+                head: None,
+                tail: None,
+            }
+        }
+
+        fn get(&mut self, key: i32) -> i32 {
+            if let Some(entry) = self.map.get(&key).cloned() {
+                let value: i32 = entry.borrow().value;
+                if !self.head.eq(&Some(entry.clone())) {
+                    self.replace_head(entry);
+                }
+                return value;
+            }
+            Self::NOT_FOUND
+        }
+
+        fn put(&mut self, key: i32, value: i32) {
+            if self.map.is_empty() {
+                let node = Node::new(key, value);
+                self.tail = Some(node.clone());
+                self.map.insert(key, node.clone());
+                self.head = Some(node);
+                return;
+            }
+
+            if !self.map.contains_key(&key) && self.map.len() >= self.capacity {
+                while self.map.len() >= self.capacity {
+                    Self::remove_tail(self);
+                }
+            }
+
+            match self.map.get(&key) {
+                Some(node) => {
+                    node.borrow_mut().value = value;
+                    Self::replace_head(self, node.clone());
+                }
+                None => {
+                    let node: LinkedListNode = Node::new(key, value);
+                    self.map.insert(key, node.clone());
+                    Self::replace_head(self, node);
+                }
+            }
+        }
+
+        fn remove_tail(&mut self) {
+            if self.head.eq(&self.tail) {
+                let key: i32 = self.tail.take().unwrap().borrow().key;
+                self.head = None;
+                self.tail = None;
+                self.map.remove(&key);
+                return;
+            }
+
+            let Some(tail) = self.tail.take() else {
+                return;
+            };
+
+            self.map.remove(&tail.borrow().key);
+
+            if tail.borrow().next.is_some() {
+                panic!("Invalid state: tail has a next");
+            }
+
+            let Some(previous) = tail.borrow_mut().previous.take() else {
+                self.tail = None;
+                return;
+            };
+            previous.borrow_mut().next = None;
+            self.tail = Some(previous);
+        }
+
+        fn replace_head(&mut self, node: LinkedListNode) {
+            // point the previous to next
+            // take head and point node to it
+            if self.head.eq(&Some(node.clone())) {
+                return;
+            }
+
+            let is_tail: bool = self.tail.eq(&Some(node.clone()));
+
+            let next = node.borrow_mut().next.take();
+            if let Some(previous) = node.borrow_mut().previous.take() {
+                previous.borrow_mut().next = next.clone();
+                if let Some(next) = next {
+                    next.borrow_mut().previous = Some(previous.clone());
+                };
+                if is_tail {
+                    self.tail = Some(previous);
+                }
+            }
+            match self.head.take() {
+                Some(head) => {
+                    head.borrow_mut().previous = Some(node.clone());
+                    node.borrow_mut().next = Some(head);
+                    self.head = Some(node);
+                }
+                None => {
+                    self.map.insert(node.borrow().key, node.clone());
+                    self.tail = Some(node.clone());
+                    self.head = Some(node);
+                }
+            }
+        }
     }
 }
 
 #[allow(dead_code)]
-impl LRUCache {
-    const NOT_FOUND: i32 = -1i32;
-
-    fn new(capacity: i32) -> Self {
-        let capacity: usize = capacity as usize;
-        Self {
-            capacity,
-            map: HashMap::with_capacity(capacity),
-            head: None,
-            tail: None,
-        }
+pub mod bad {
+    use std::collections::{HashMap, VecDeque};
+    struct LRUCache {
+        capacity: usize,
+        map: HashMap<i32, i32>,
+        list: VecDeque<i32>,
     }
 
-    fn get(&mut self, key: i32) -> i32 {
-        if let Some(entry) = self.map.get(&key).cloned() {
-            let value: i32 = entry.borrow().value;
-            if !self.head.eq(&Some(entry.clone())) {
-                self.replace_head(entry);
-            }
-            return value;
-        }
-        Self::NOT_FOUND
-    }
-
-    fn put(&mut self, key: i32, value: i32) {
-        if self.map.is_empty() {
-            let node = Node::new(key, value);
-            self.tail = Some(node.clone());
-            self.map.insert(key, node.clone());
-            self.head = Some(node);
-            return;
-        }
-
-        if !self.map.contains_key(&key) && self.map.len() >= self.capacity {
-            while self.map.len() >= self.capacity {
-                Self::remove_tail(self);
+    impl LRUCache {
+        fn new(capacity: i32) -> Self {
+            let capacity: usize = capacity as usize;
+            Self {
+                capacity,
+                map: HashMap::with_capacity(capacity),
+                list: VecDeque::with_capacity(capacity),
             }
         }
 
-        match self.map.get(&key) {
-            Some(node) => {
-                node.borrow_mut().value = value;
-                Self::replace_head(self, node.clone());
-            }
-            None => {
-                let node: LinkedListNode = Node::new(key, value);
-                self.map.insert(key, node.clone());
-                Self::replace_head(self, node);
-            }
-        }
-    }
-
-    fn remove_tail(&mut self) {
-        if self.head.eq(&self.tail) {
-            let key: i32 = self.tail.take().unwrap().borrow().key;
-            self.head = None;
-            self.tail = None;
-            self.map.remove(&key);
-            return;
-        }
-
-        let Some(tail) = self.tail.take() else {
-            return;
-        };
-
-        self.map.remove(&tail.borrow().key);
-
-        if tail.borrow().next.is_some() {
-            panic!("Invalid state: tail has a next");
-        }
-
-        let Some(previous) = tail.borrow_mut().previous.take() else {
-            self.tail = None;
-            return;
-        };
-        previous.borrow_mut().next = None;
-        self.tail = Some(previous);
-    }
-
-    fn replace_head(&mut self, node: LinkedListNode) {
-        // point the previous to next
-        // take head and point node to it
-        if self.head.eq(&Some(node.clone())) {
-            return;
-        }
-
-        let is_tail: bool = self.tail.eq(&Some(node.clone()));
-
-        let next = node.borrow_mut().next.take();
-        if let Some(previous) = node.borrow_mut().previous.take() {
-            previous.borrow_mut().next = next.clone();
-            if let Some(next) = next {
-                next.borrow_mut().previous = Some(previous.clone());
+        fn touch(&mut self, key: i32) {
+            let Some(index) = self.list.iter().position(|k| *k == key) else {
+                self.list.push_front(key);
+                return;
             };
-            if is_tail {
-                self.tail = Some(previous);
-            }
+
+            self.list.remove(index);
+            self.list.push_front(key);
         }
-        match self.head.take() {
-            Some(head) => {
-                head.borrow_mut().previous = Some(node.clone());
-                node.borrow_mut().next = Some(head);
-                self.head = Some(node);
-            }
-            None => {
-                self.map.insert(node.borrow().key, node.clone());
-                self.tail = Some(node.clone());
-                self.head = Some(node);
-            }
+
+        fn get(&mut self, key: i32) -> i32 {
+            let Some(value) = self.map.get(&key).cloned() else {
+                return -1i32;
+            };
+            self.touch(key);
+            value
+        }
+
+        fn put(&mut self, key: i32, value: i32) {
+            if !self.map.contains_key(&key) {
+                if self.list.len() >= self.capacity {
+                    let remove: i32 = self.list.pop_back().unwrap();
+                    self.map.remove(&remove);
+                }
+                self.map.insert(key, value);
+                self.list.push_front(key);
+                return;
+            };
+            self.map.remove(&key);
+            self.map.insert(key, value);
+            self.touch(key);
         }
     }
 }
