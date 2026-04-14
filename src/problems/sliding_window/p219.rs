07816ceb5f7be7de2d7842d646cd04bf29365db7
seanpmyers <spmyers@protonmail.com>
2026-04-14 16:28:02 -0400

    adding p219 solution

--- src/problems/sliding_window/p219.rs
+++ src/problems/sliding_window/p219.rs
@@ -1,4 +1,42 @@
-pub mod sliding_window {
+pub mod linear_hash_set {
+    pub struct Solution;
+    use std::collections::HashSet;
+    impl Solution {
+        pub fn contains_nearby_duplicate(nums: Vec<i32>, k: i32) -> bool {
+            if nums.len() <= 1 {
+                return false;
+            }
+
+            let k: usize = k as usize;
+            let mut set: HashSet<i32> = HashSet::with_capacity(k + 1);
+
+            let mut l: usize = 0;
+            let mut r: usize = 1;
+            set.insert(nums[l]);
+
+            while r < nums.len() && l.abs_diff(r) < k {
+                if !set.insert(nums[r]) {
+                    return true;
+                }
+                r += 1;
+            }
+
+            while l < nums.len() && r < nums.len() {
+                if l != r && !set.insert(nums[r]) && l.abs_diff(r) <= k {
+                    return true;
+                }
+
+                set.remove(&nums[l]);
+                l += 1;
+                r += 1;
+            }
+
+            false
+        }
+    }
+}
+
+pub mod sliding_window_brute_force {
     pub struct Solution;
     impl Solution {
         pub fn contains_nearby_duplicate(nums: Vec<i32>, k: i32) -> bool {
