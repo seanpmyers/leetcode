7f1fdc3b0c8e8e6cdc00d26d7d793b6ee2d06159
seanpmyers <spmyers@protonmail.com>
2026-04-23 07:32:08 -0400

    adding p238 revision

--- src/problems/arrays_and_hashing/p238.rs
+++ src/problems/arrays_and_hashing/p238.rs
@@ -1,22 +1,44 @@
 // #arrays #hashing
-pub struct Solution {}
-impl Solution {
-    pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
-        let mut result: Vec<i32> = vec![1i32; nums.len()];
-        let len: usize = nums.len() - 1;
-        let mut left: i32 = 1;
-        let mut right: i32 = 1;
+pub mod clean {
+    pub struct Solution;
+    impl Solution {
+        pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
+            let mut result: Vec<i32> = vec![1i32; nums.len()];
+            let mut left: i32 = 1;
+            let mut right: i32 = 1;
+            let len: usize = nums.len() - 1;
+            for i in 1..nums.len() {
+                left *= nums[i - 1];
+                right *= nums[len - i + 1];
+                result[i] *= left;
+                result[len - i] *= right;
+            }
 
-        for i in 0..len {
-            result[i] *= left;
-            result[len - i] *= right;
-            left *= nums[i];
-            right *= nums[len - i];
+            result
         }
+    }
+}
 
-        result[0] *= right;
-        result[len] *= left;
+pub mod original {
+    pub struct Solution {}
+    impl Solution {
+        pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
+            let mut result: Vec<i32> = vec![1i32; nums.len()];
+            let len: usize = nums.len() - 1;
+            let mut left: i32 = 1;
+            let mut right: i32 = 1;
 
-        result
+            for i in 0..len {
+                result[i] *= left;
+                result[len - i] *= right;
+                left *= nums[i];
+                right *= nums[len - i];
+            }
+
+            result[0] *= right;
+            result[len] *= left;
+
+            result
+        }
     }
 }
