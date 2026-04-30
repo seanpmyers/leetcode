1042655eaa57f9452cf8b9535c8ffc0aa4550e82
seanpmyers <spmyers@protonmail.com>
2026-04-30 17:14:36 -0400

    adding p128 solution

--- src/problems/arrays_and_hashing/p128.rs
+++ src/problems/arrays_and_hashing/p128.rs
@@ -109,3 +109,79 @@ pub mod disjoint_set_union {
         }
     }
 }
+
+pub mod disjoint_set_union2 {
+    pub struct Solution;
+    use std::cmp::Ordering;
+    use std::collections::HashMap;
+    pub struct UnionFind {
+        pub parents: Vec<usize>,
+        pub ranks: Vec<usize>,
+        pub max: i32,
+    }
+
+    impl UnionFind {
+        pub fn new(n: usize) -> Self {
+            Self {
+                parents: (0..n).collect(),
+                ranks: vec![1; n],
+                max: if n > 0 { 1 } else { 0 },
+            }
+        }
+
+        pub fn find(&mut self, x: usize) -> usize {
+            let mut p: usize = x;
+
+            while p != self.parents[p] {
+                self.parents[p] = self.parents[self.parents[p]];
+                p = self.parents[p];
+            }
+
+            p
+        }
+
+        pub fn union(&mut self, x: usize, y: usize) {
+            let x = self.find(x);
+            let y = self.find(y);
+
+            if x == y {
+                return;
+            }
+
+            match self.ranks[x].cmp(&self.ranks[y]) {
+                Ordering::Greater | Ordering::Equal => {
+                    self.ranks[x] += self.ranks[y];
+                    self.parents[y] = x;
+                    self.max = self.max.max(self.ranks[x] as i32);
+                }
+                Ordering::Less => {
+                    self.ranks[y] += self.ranks[x];
+                    self.parents[x] = y;
+                    self.max = self.max.max(self.ranks[y] as i32);
+                }
+            }
+        }
+    }
+
+    impl Solution {
+        pub fn longest_consecutive(nums: Vec<i32>) -> i32 {
+            let mut map: HashMap<i32, usize> = HashMap::new();
+            let mut dsu: UnionFind = UnionFind::new(nums.len());
+
+            for (i, n) in nums.into_iter().enumerate() {
+                if map.contains_key(&n) {
+                    continue;
+                }
+                if let Some(next) = map.get(&(n + 1)).cloned() {
+                    dsu.union(next, i);
+                }
+                if let Some(previous) = map.get(&(n - 1)).cloned() {
+                    dsu.union(previous, i);
+                }
+                map.insert(n, i);
+            }
+
+            dsu.max
+        }
+    }
+}
