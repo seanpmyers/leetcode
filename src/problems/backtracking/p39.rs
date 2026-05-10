8420a15d3396be66747eebdd23f7149f9c5751a8
seanpmyers <spmyers@protonmail.com>
2026-05-10 09:57:07 -0400

    adding p39 solution

--- src/problems/backtracking/p39.rs
+++ src/problems/backtracking/p39.rs
@@ -1,3 +1,43 @@
+pub mod optimal_sort_prune {
+    pub struct Solution;
+    impl Solution {
+        pub fn combination_sum(candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
+            let mut result: Vec<Vec<i32>> = Vec::with_capacity(150usize);
+            let mut nums = candidates;
+            nums.sort();
+            Self::dfs(&mut result, target, &mut vec![], 0i32, 0usize, &nums);
+
+            result
+        }
+
+        pub fn dfs(
+            result: &mut Vec<Vec<i32>>,
+            target: i32,
+            current: &mut Vec<i32>,
+            sum: i32,
+            i: usize,
+            nums: &Vec<i32>,
+        ) {
+            if sum == target {
+                result.push(current.clone());
+                return;
+            }
+
+            if sum > target || i >= nums.len() {
+                return;
+            }
+
+            for j in i..nums.len() {
+                if sum + nums[j] > target {
+                    return;
+                }
+                current.push(nums[j]);
+                Self::dfs(result, target, current, sum + nums[j], j, nums);
+                current.pop();
+            }
+        }
+    }
+}
 pub mod backtracking {
     pub struct Solution;
     impl Solution {
