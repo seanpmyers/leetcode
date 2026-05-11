74ba8f5db46e4827f84dd929728474ab85bdbcbb
seanpmyers <spmyers@protonmail.com>
2026-05-11 07:14:37 -0400

    adding p46 solution

--- src/problems/backtracking/p46.rs
+++ src/problems/backtracking/p46.rs
@@ -1,33 +1,57 @@
-pub struct Solution;
+pub mod iterative {
+    pub struct Solution;
+    impl Solution {
+        pub fn permute(nums: Vec<i32>) -> Vec<Vec<i32>> {
+            let mut result: Vec<Vec<i32>> = vec![vec![]];
 
-impl Solution {
-    pub fn permute(nums: Vec<i32>) -> Vec<Vec<i32>> {
-        let mut result: Vec<Vec<i32>> = vec![];
-
-        Self::backtrack(&mut result, &nums, 0, vec![]);
-
-        result
-    }
-
-    pub fn backtrack(
-        result: &mut Vec<Vec<i32>>,
-        input: &[i32],
-        index: usize,
-        mut current: Vec<i32>,
-    ) {
-        if index >= input.len() {
-            if input.is_empty() {
-                result.push(current);
+            for &n in nums.iter() {
+                let mut perms: Vec<Vec<i32>> = vec![];
+                for p in &result {
+                    for i in 0..p.len() + 1 {
+                        let mut copy = p.clone();
+                        copy.insert(i, n);
+                        perms.push(copy);
+                    }
+                }
+                result = perms;
             }
-            return;
+
+            result
+        }
+    }
+}
+pub mod backtracking {
+    pub struct Solution;
+
+    impl Solution {
+        pub fn permute(nums: Vec<i32>) -> Vec<Vec<i32>> {
+            let mut result: Vec<Vec<i32>> = vec![];
+
+            Self::backtrack(&mut result, &nums, 0, vec![]);
+
+            result
         }
 
-        let mut x = input.to_vec();
-        let value = x.remove(index);
-        current.push(value);
-        Self::backtrack(result, &x, 0, current.clone());
-        current.pop();
+        pub fn backtrack(
+            result: &mut Vec<Vec<i32>>,
+            input: &[i32],
+            index: usize,
+            mut current: Vec<i32>,
+        ) {
+            if index >= input.len() {
+                if input.is_empty() {
+                    result.push(current);
+                }
+                return;
+            }
 
-        Self::backtrack(result, input, index.saturating_add(1), current);
+            let mut x = input.to_vec();
+            let value = x.remove(index);
+            current.push(value);
+            Self::backtrack(result, &x, 0, current.clone());
+            current.pop();
+
+            Self::backtrack(result, input, index.saturating_add(1), current);
+        }
     }
 }
