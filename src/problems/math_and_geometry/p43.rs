70c781846c1aa6740b4c31ab3109cb950204eb85
seanpmyers <spmyers@protonmail.com>
2026-07-13 09:17:34 -0400

    adding p43

--- src/problems/math_and_geometry/p43.rs
+++ src/problems/math_and_geometry/p43.rs
@@ -1,45 +1,83 @@
-pub struct Solution;
-impl Solution {
-    pub fn multiply(num1: String, num2: String) -> String {
-        if num1 == "0" || num2 == "0" {
-            return "0".to_string();
-        }
-
-        let n1 = num1.len();
-        let n2 = num2.len();
-        let mut res = vec![0; n1 + n2];
-
-        let b1 = num1.as_bytes();
-        let b2 = num2.as_bytes();
-
-        for i in (0..n1).rev() {
-            for j in (0..n2).rev() {
-                let idx1 = n1 - 1 - i;
-                let idx2 = n2 - 1 - j;
-
-                let digit = (b1[i] - b'0') * (b2[j] - b'0');
-
-                res[idx1 + idx2] += digit as i32;
-                res[idx1 + idx2 + 1] += res[idx1 + idx2] / 10;
-                res[idx1 + idx2] %= 10;
+pub mod second {
+    pub struct Solution;
+    impl Solution {
+        pub fn multiply(num1: String, num2: String) -> String {
+            if num1 == "0" || num2 == "0" {
+                return "0".to_string();
             }
-        }
 
-        let mut result = String::new();
-        let mut i = res.len() - 1;
+            let x: &[u8] = num1.as_bytes();
+            let y: &[u8] = num2.as_bytes();
 
-        while i > 0 && res[i] == 0 {
-            i -= 1;
-        }
+            let mut result: Vec<u8> = vec![0; x.len() + y.len()];
 
-        while i < res.len() {
-            result.push_str(&res[i].to_string());
-            if i == 0 {
-                break;
+            for i in 0..x.len() {
+                let ii: usize = x.len() - i - 1;
+                let a: u8 = x[ii] - b'0';
+
+                for j in 0..y.len() {
+                    let jj: usize = y.len() - j - 1;
+                    let b: u8 = y[jj] - b'0';
+
+                    let mul = (a * b) + result[ii + jj + 1];
+
+                    result[ii + jj + 1] = mul % 10;
+                    result[ii + jj] += mul / 10;
+                }
             }
-            i -= 1;
-        }
 
-        result
+            result
+                .into_iter()
+                .skip_while(|&x| x == 0)
+                .map(|x| (x + b'0') as char)
+                .collect()
+        }
+    }
+}
+pub mod first {
+    pub struct Solution;
+    impl Solution {
+        pub fn multiply(num1: String, num2: String) -> String {
+            if num1 == "0" || num2 == "0" {
+                return "0".to_string();
+            }
+
+            let n1 = num1.len();
+            let n2 = num2.len();
+            let mut res = vec![0; n1 + n2];
+
+            let b1 = num1.as_bytes();
+            let b2 = num2.as_bytes();
+
+            for i in (0..n1).rev() {
+                for j in (0..n2).rev() {
+                    let idx1 = n1 - 1 - i;
+                    let idx2 = n2 - 1 - j;
+
+                    let digit = (b1[i] - b'0') * (b2[j] - b'0');
+
+                    res[idx1 + idx2] += digit as i32;
+                    res[idx1 + idx2 + 1] += res[idx1 + idx2] / 10;
+                    res[idx1 + idx2] %= 10;
+                }
+            }
+
+            let mut result = String::new();
+            let mut i = res.len() - 1;
+
+            while i > 0 && res[i] == 0 {
+                i -= 1;
+            }
+
+            while i < res.len() {
+                result.push_str(&res[i].to_string());
+                if i == 0 {
+                    break;
+                }
+                i -= 1;
+            }
+
+            result
+        }
     }
 }
