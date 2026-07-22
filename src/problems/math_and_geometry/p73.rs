0a81cec179b3a15347d05c8b18cd235bf8da6c26
seanpmyers <spmyers@protonmail.com>
2026-07-22 07:46:34 -0400

    adding p73 solution variant

--- src/problems/math_and_geometry/p73.rs
+++ src/problems/math_and_geometry/p73.rs
@@ -1,40 +1,89 @@
-pub struct Solution;
-impl Solution {
-    pub fn set_zeroes(matrix: &mut Vec<Vec<i32>>) {
-        let rows: usize = matrix.len();
-        let columns: usize = matrix[0].len();
-        let mut row_zero = false;
+pub mod updated {
+    pub struct Solution;
+    impl Solution {
+        pub fn set_zeroes(matrix: &mut Vec<Vec<i32>>) {
+            let rows: usize = matrix.len();
+            let columns: usize = matrix[0].len();
 
-        for r in 0..rows {
-            for c in 0..columns {
-                if matrix[r][c] == 0 {
+            let mut z: bool = false;
+
+            for r in 0..rows {
+                for c in 0..columns {
+                    if matrix[r][c] != 0 {
+                        continue;
+                    }
                     matrix[0][c] = 0;
                     if r > 0 {
                         matrix[r][0] = 0;
-                    } else {
-                        row_zero = true;
+                        continue;
                     }
+                    z = true;
                 }
             }
-        }
 
-        for r in 1..rows {
-            for c in 1..columns {
-                if matrix[0][c] == 0 || matrix[r][0] == 0 {
+            for r in 1..rows {
+                for c in 1..columns {
+                    if matrix[0][c] != 0 && matrix[r][0] != 0 {
+                        continue;
+                    }
                     matrix[r][c] = 0;
                 }
             }
-        }
 
-        if matrix[0][0] == 0 {
-            for r in 0..rows {
-                matrix[r][0] = 0;
+            if matrix[0][0] == 0 {
+                for r in 0..rows {
+                    matrix[r][0] = 0;
+                }
             }
-        }
 
-        if row_zero {
+            if !z {
+                return;
+            }
             for c in 0..columns {
                 matrix[0][c] = 0;
+            }
+        }
+    }
+}
+pub mod space_optimized {
+    pub struct Solution;
+    impl Solution {
+        pub fn set_zeroes(matrix: &mut Vec<Vec<i32>>) {
+            let rows: usize = matrix.len();
+            let columns: usize = matrix[0].len();
+            let mut row_zero = false;
+
+            for r in 0..rows {
+                for c in 0..columns {
+                    if matrix[r][c] == 0 {
+                        matrix[0][c] = 0;
+                        if r > 0 {
+                            matrix[r][0] = 0;
+                        } else {
+                            row_zero = true;
+                        }
+                    }
+                }
+            }
+
+            for r in 1..rows {
+                for c in 1..columns {
+                    if matrix[0][c] == 0 || matrix[r][0] == 0 {
+                        matrix[r][c] = 0;
+                    }
+                }
+            }
+
+            if matrix[0][0] == 0 {
+                for r in 0..rows {
+                    matrix[r][0] = 0;
+                }
+            }
+
+            if row_zero {
+                for c in 0..columns {
+                    matrix[0][c] = 0;
+                }
             }
         }
     }
