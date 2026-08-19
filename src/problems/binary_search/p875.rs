2ce8d28ff5cc42698a4cfef93eba7543c2d0bade
seanpmyers <spmyers@protonmail.com>
2026-08-19 07:27:44 -0400

    adding p875 solution

--- src/problems/binary_search/p875.rs
+++ src/problems/binary_search/p875.rs
@@ -1,27 +1,61 @@
-pub struct Solution {}
+pub mod second {
+    pub struct Solution;
+    use std::cmp::Ordering;
+    impl Solution {
+        pub fn min_eating_speed(piles: Vec<i32>, h: i32) -> i32 {
+            let max: i32 = *piles.iter().max().unwrap_or(&i32::MAX);
+            let mut result: i32 = max;
+            let mut l: i32 = 0;
+            let mut r: i32 = max;
 
-impl Solution {
-    pub fn min_eating_speed(piles: Vec<i32>, h: i32) -> i32 {
-        let calc = |k| {
-            piles.iter().fold(0, |accumulator, x| {
-                accumulator + (x / k) + if x % k > 0 { 1 } else { 0 }
-            })
-        };
-        let mut start: i32 = 1i32;
-        let mut end: i32 = *piles.iter().max().unwrap_or(&i32::MAX);
+            while l <= r {
+                let k: f64 = l.midpoint(r) as f64;
+                let mut hours: i32 = 0;
 
-        while start < end {
-            let middle = middle(start, end);
-            if calc(middle) <= h {
-                end = middle;
-            } else {
-                start = middle + 1;
+                for pile in piles.iter().map(|p| *p as f64) {
+                    hours = hours.saturating_add((pile / k).ceil() as i32);
+                }
+                let k = k as i32;
+
+                match hours.cmp(&h) {
+                    Ordering::Equal | Ordering::Less => {
+                        result = result.min(k);
+                        r = k - 1;
+                    }
+                    Ordering::Greater => l = k + 1,
+                };
             }
+
+            result
         }
-        start
     }
 }
+pub mod first {
+    pub struct Solution {}
 
-pub fn middle(start: i32, end: i32) -> i32 {
-    start + (end - start) / 2i32
+    impl Solution {
+        pub fn min_eating_speed(piles: Vec<i32>, h: i32) -> i32 {
+            let calc = |k| {
+                piles.iter().fold(0, |accumulator, x| {
+                    accumulator + (x / k) + if x % k > 0 { 1 } else { 0 }
+                })
+            };
+            let mut start: i32 = 1i32;
+            let mut end: i32 = *piles.iter().max().unwrap_or(&i32::MAX);
+
+            while start < end {
+                let middle = middle(start, end);
+                if calc(middle) <= h {
+                    end = middle;
+                } else {
+                    start = middle + 1;
+                }
+            }
+            start
+        }
+    }
+
+    pub fn middle(start: i32, end: i32) -> i32 {
+        start + (end - start) / 2i32
+    }
 }
