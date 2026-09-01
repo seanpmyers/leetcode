63ad17dca0356646c65d09439ee4cc8482830b9e
seanpmyers <spmyers@protonmail.com>
2026-09-01 06:52:58 -0400

    adding p110 solution

--- src/problems/trees/p110.rs
+++ src/problems/trees/p110.rs
@@ -1,3 +1,49 @@
+pub mod recursive_2 {
+
+    // Definition for a binary tree node.
+    #[derive(Debug, PartialEq, Eq)]
+    pub struct TreeNode {
+        pub val: i32,
+        pub left: Option<Rc<RefCell<TreeNode>>>,
+        pub right: Option<Rc<RefCell<TreeNode>>>,
+    }
+
+    impl TreeNode {
+        #[inline]
+        pub fn new(val: i32) -> Self {
+            TreeNode {
+                val,
+                left: None,
+                right: None,
+            }
+        }
+    }
+    use std::cell::RefCell;
+    use std::rc::Rc;
+    pub struct Solution;
+    impl Solution {
+        pub fn is_balanced(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
+            let (left, right, diff) = x(root);
+            left.abs_diff(right) <= 1 && diff <= 1
+        }
+    }
+
+    pub fn x(root: Option<Rc<RefCell<TreeNode>>>) -> (u16, u16, u16) {
+        let Some(root) = root else {
+            return (0, 0, 0);
+        };
+        let mut diff = 0;
+        let left = x(root.borrow_mut().left.take());
+        diff = diff.max(left.2);
+        let left = left.0.max(left.1);
+
+        let right = x(root.borrow_mut().right.take());
+        diff = diff.max(right.2);
+        let right = right.0.max(right.1);
+        diff = diff.max(left.abs_diff(right));
+        (left + 1, right + 1, diff)
+    }
+}
 pub mod iterative {
     // Definition for a binary tree node.
     #[derive(Debug, PartialEq, Eq)]
