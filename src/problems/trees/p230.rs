58b7e2550be35ffac96267cb364e1f06f0c9d40e
Sean Myers <spmyers@protonmail.com>
2026-09-11 06:14:20 -0400

    adding p230 solution

--- src/problems/trees/p230.rs
+++ src/problems/trees/p230.rs
@@ -1,3 +1,57 @@
+#[allow(dead_code)]
+pub mod stack {
+    // Definition for a binary tree node.
+    #[derive(Debug, PartialEq, Eq)]
+    pub struct TreeNode {
+        pub val: i32,
+        pub left: Option<Rc<RefCell<TreeNode>>>,
+        pub right: Option<Rc<RefCell<TreeNode>>>,
+    }
+
+    impl TreeNode {
+        #[inline]
+        pub fn new(val: i32) -> Self {
+            TreeNode {
+                val,
+                left: None,
+                right: None,
+            }
+        }
+    }
+    use std::cell::RefCell;
+    use std::rc::Rc;
+    pub struct Solution;
+    impl Solution {
+        pub fn kth_smallest(root: Option<Rc<RefCell<TreeNode>>>, k: i32) -> i32 {
+            let Some(root) = root else {
+                return 0;
+            };
+            let mut stack: Vec<Rc<RefCell<TreeNode>>> = vec![root];
+            let mut count: i32 = 0;
+            while let Some(node) = stack.pop() {
+                let left = node.borrow_mut().left.take();
+                let right = node.borrow_mut().right.take();
+                if left.is_none() && right.is_none() {
+                    count += 1;
+                    if count == k {
+                        return node.borrow().val;
+                    }
+                    continue;
+                }
+
+                if let Some(right) = right {
+                    stack.push(right);
+                }
+                stack.push(node);
+                if let Some(left) = left {
+                    stack.push(left);
+                }
+            }
+
+            0i32
+        }
+    }
+}
 pub mod optimal {
     use std::{cell::RefCell, rc::Rc};
 
